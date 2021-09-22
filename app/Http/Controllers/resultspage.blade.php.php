1382a458d6c6002b8fc9fa14eeeba6185f2314bc
abayataceleste@gmail.com <marian.cuares26@gmail.com>
2021-09-22 16:05:31 +0800

    lesson10actvity2

---
+++ app/Http/Controllers/resultspage.blade.php.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class resultspage.blade.php extends Controller
+{
+    <!DOCTYPE html>
+<html>
+<head>
+<title> {{ $title }}</title>
+</head>
+<body>
+    <h1> The result is: {{$result}}</h1>
+}
